d5183c66276b18a99b721bd51e3ef28b8a22b165
Seonggil Jeong <sgjeong1108@icloud.com>
2025-10-28 15:44:25 +0900

    [#6]feat: impl EV charging sessions table pipeline
    # Conflicts:
    #	src/devcourse_project2/__init__.py
    #	src/pipelines/s3_upload_pipeline.py

--- src/devcourse_project2/__init__.py
+++ src/devcourse_project2/__init__.py
@@ -1,3 +1,6 @@
+from pipelines.fact_session_table_pipeline import (
+    run_create_ev_charging_sessions_table_pipeline,
+)
 from pipelines.s3_upload_pipeline import run_s3_upload_pipeline
 
 
@@ -7,3 +10,4 @@ def main():
     run_session_stage_pipeline()
     run_region_info_pipeline()
     run_snowflake_pipeline()
+    run_create_ev_charging_sessions_table_pipeline()
--- src/pipelines/s3_upload_pipeline.py
+++ src/pipelines/s3_upload_pipeline.py
@@ -1,26 +1,38 @@
 import logging
 import os
-from typing import List
+from typing import (
+    List,
+)
 
-from common.base_tasks import Task
-from utils.conn_utils import get_s3_conn
-from utils.file_utils import get_path
+from common.base_tasks import (
+    Task,
+)
+from utils.conn_utils import (
+    get_s3_conn,
+)
+from utils.file_utils import (
+    get_path,
+)
 
 
 def convert_session_data_to_csv(years: List[str]):
     output_paths = []
     rename_columns = {
-        '충전소명': 'station_name',
-        '충전시작시간': 'start_time',
-        '충전종료시간': 'end_time',
-        '충전량(kW)': 'charged_kwh'
+        "충전소명": "station_name",
+        "충전시작시간": "start_time",
+        "충전종료시간": "end_time",
+        "충전량(kW)": "charged_kwh",
     }
     file_paths = [
-        get_path(f"data/sessions/year={year}/ev_charging_session.xlsx") for
-        year in years]
+        get_path(f"data/sessions/year={year}/ev_charging_session.xlsx")
+        for year in years
+    ]
 
     for file in file_paths:
-        from utils.file_utils import xlsx_to_csv
+        from utils.file_utils import (
+            xlsx_to_csv,
+        )
+
         out_path = file.replace(".xlsx", ".csv")
 
         xlsx_to_csv(file, out_path, skip_rows=3, rename_columns=rename_columns)
@@ -32,21 +44,25 @@ def convert_session_data_to_csv(years: List[str]):
 def convert_station_data_to_csv(station_files: List[str]):
     output_paths = []
     rename_columns = {
-        '충전소명': 'station_name',
-        '주소': 'address',
-        '위도': 'latitude',
-        '경도': 'longitude',
-        '충전소 용량(kW)': 'capacity_kw',
-        '전체': 'total_chargers',
-        '완속': 'slow_chargers',
-        '급속': 'fast_chargers',
-        '콘센트': 'outlet_chargers'
+        "충전소명": "station_name",
+        "주소": "address",
+        "위도": "latitude",
+        "경도": "longitude",
+        "충전소 용량(kW)": "capacity_kw",
+        "전체": "total_chargers",
+        "완속": "slow_chargers",
+        "급속": "fast_chargers",
+        "콘센트": "outlet_chargers",
     }
-    file_paths = [get_path(f"data/stations/{stations}.xlsx")
-                  for stations in station_files]
+    file_paths = [
+        get_path(f"data/stations/{stations}.xlsx") for stations in station_files
+    ]
 
     for file in file_paths:
-        from utils.file_utils import xlsx_to_csv
+        from utils.file_utils import (
+            xlsx_to_csv,
+        )
+
         out_path = file.replace(".xlsx", ".csv")
         xlsx_to_csv(file, out_path, skip_rows=3, rename_columns=rename_columns)
         output_paths.append(out_path)
@@ -55,37 +71,31 @@ def convert_station_data_to_csv(station_files: List[str]):
 
 def upload_to_s3(local_paths: List[str]):
     s3 = get_s3_conn()
-    BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 
     for path in local_paths:
         key = path.split("data/")[-1]
         s3_key = f"data/{key}"
 
         try:
-            s3.upload_file(path, BUCKET_NAME, s3_key)
-            print(f"업로드 완료: s3://{BUCKET_NAME}/{s3_key}")
+            s3.upload_file(path, os.environ.get("S3_BUCKET_NAME"), s3_key)
         except Exception as e:
-            logging.warning(f"Failed to upload {path} to S3: {e}")
+            logging.log(logging.ERROR, f"Failed to upload {path} to S3: {e}")
 
 
-convert_session_task = Task("convert_session_task",
-                            lambda: convert_session_data_to_csv(
-                                ["2024", "2025"]))
-convert_station_task = Task("convert_station_task",
-                            lambda: convert_station_data_to_csv(
-                                ['seoul_station']))
+convert_session_task = Task(
+    "convert_session_task", lambda: convert_session_data_to_csv(["2024", "2025"])
+)
+convert_station_task = Task(
+    "convert_station_task", lambda: convert_station_data_to_csv(["seoul_station"])
+)
 
-upload_to_s3_task = Task("upload_to_s3_task",
-                         lambda: upload_to_s3(convert_session_task.result
-                                              + convert_station_task.result))
+upload_to_s3_task = Task(
+    "upload_to_s3_task",
+    lambda: upload_to_s3(convert_session_task.result + convert_station_task.result),
+)
 
 convert_station_task >> convert_session_task >> upload_to_s3_task
 
 
 def run_s3_upload_pipeline():
     convert_station_task.run()
-    
-if __name__ == "__main__":
-    print("S3 업로드 파이프라인 시작")
-    run_s3_upload_pipeline()
-    print(" S3 업로드 파이프라인 완료")
